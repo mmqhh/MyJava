1d1ac3990f7344b37f6c268735e32ae59cf20568
mmqhh <mmqhh@hotmail.com>
2015-09-21 21:11:35 +0800

    add GetCurrentThread.java

---
+++ src/GetCurrentThread.java
@@ -0,0 +1,15 @@
+//Getting the current thread and displaying its info
+public class GetCurrentThread {
+
+	public static void main(String[] args) {
+		Thread t = Thread.currentThread();
+		t.setName("MainThread");
+		System.out.println("Id of current thread is: "+t.getId());
+		System.out.println("Name of current thread is: "+t.getName());
+		System.out.println("Priority of current thread is: "+t.getPriority());
+		}
+	}
+
+
+
+
