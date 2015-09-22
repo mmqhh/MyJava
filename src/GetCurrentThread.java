a64c6ef282bbcc8d3c0d30824a175715f7b6abe1
mmqhh <mmqhh@hotmail.com>
2015-09-22 10:59:44 +0800

    Add function add

--- src/GetCurrentThread.java
+++ src/GetCurrentThread.java
@@ -1,4 +1,4 @@
-//Getting the current thread and displaying its info
+import java.util.*;
 public class GetCurrentThread {
 
 	public static void main(String[] args) {
@@ -7,7 +7,15 @@ public class GetCurrentThread {
 		System.out.println("Id of current thread is: "+t.getId());
 		System.out.println("Name of current thread is: "+t.getName());
 		System.out.println("Priority of current thread is: "+t.getPriority());
+		System.out.println("Enter 2 numbers to add");
+		System.out.println(add(1,2));
 		}
+	static int add(int x,int y){
+		Scanner in = new Scanner(System.in);
+		int a = in.nextInt();
+		int b = in.nextInt();
+		return a+b;
+	}
 	}
 
 
